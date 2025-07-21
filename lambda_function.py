f2e02312114a683763c1d00995ee53be046f6146
akshzt123 <akshat005jain@gmail.com>
2025-07-22 02:52:11 +0530

    Update greeting logic in Lambda

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,6 @@
-def handler(event, context):
+def lambda_handler(event, context):
+    name = event.get('name', 'World')
     return {
-        "statusCode": 200,
-        "body": "Hello from Lambda!"
+        'statusCode': 200,
+        'body': f'Hello, {name}!'
     }
